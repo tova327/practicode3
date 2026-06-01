c79c689f9ec00b04405966049dec69ee0f393ca0
tova327 <tovi3500@gmail.com>
2026-06-01 15:46:52 +0300

    Update axios base URL to use API_URL

--- clientside/src/service.js
+++ clientside/src/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL =`${process.env.REACT_APP_API_KEY}`;
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 // Add a response interceptor
 axios.interceptors.response.use(
